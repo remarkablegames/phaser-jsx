25ab9e2a7844fddf43abd0ecc99330b94bad598a
Mark <mark@remarkablemark.org>
2024-01-09 19:11:16 -0500

    feat: export types GameObjectProps and Ref

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './components';
 export { createElement, jsx } from './element';
 export { createRef } from './ref';
 export { render } from './render';
+export type { GameObjectProps, Ref } from './types';
